a15c6d1073515672d4536c76a5ab739f428dcc93
dmitry alexandrovsky <alexandrovsky.dmitry@gmail.com>
2015-07-24 00:15:07 +0200

    added TransformFeedback class

---
+++ heatsculpt/TransformFeedback.cpp
@@ -0,0 +1,9 @@
+//
+//  TransformFeedback.cpp
+//  heatsculpt
+//
+//  Created by Dmitry Alexandrovsky on 24.07.15.
+//  Copyright (c) 2015 Dmitry Alexandrovsky. All rights reserved.
+//
+
+#include "TransformFeedback.h"
---
+++ heatsculpt/TransformFeedback.h
@@ -0,0 +1,14 @@
+//
+//  TransformFeedback.h
+//  heatsculpt
+//
+//  Created by Dmitry Alexandrovsky on 24.07.15.
+//  Copyright (c) 2015 Dmitry Alexandrovsky. All rights reserved.
+//
+
+#ifndef __heatsculpt__TransformFeedback__
+#define __heatsculpt__TransformFeedback__
+
+#include <stdio.h>
+
+#endif /* defined(__heatsculpt__TransformFeedback__) */
